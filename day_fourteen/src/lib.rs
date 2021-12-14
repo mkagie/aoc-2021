a70cf97e76b46f1cc160d5112ce9b16e0e4d0c54
Matt Kagie <mkagie@anduril.com>
2021-12-13 22:38:27 -0800

    Fixed it

--- day_fourteen/src/lib.rs
+++ day_fourteen/src/lib.rs
@@ -23,12 +23,16 @@ impl Rule {
         }
     }
 
-    pub fn matches(&self, input: &str) -> Option<String> {
-        let output = input.replace(&self.pattern, &self.repl);
-        if input == output {
-            None
-        } else {
+    pub fn gen_new_pattern(&self, input: &str) -> Option<Vec<String>> {
+        if input == self.pattern {
+            // Create the output pattern
+            let output = vec![
+                format!("{}{}", self.pattern.chars().next().unwrap(), self.repl),
+                format!("{}{}", self.repl, self.pattern.chars().nth(1).unwrap()),
+            ];
             Some(output)
+        } else {
+            None
         }
     }
 }
@@ -39,78 +43,69 @@ fn process_inputs(input: &[String]) -> (String, Vec<Rule>) {
     (pattern, rules)
 }
 
-fn part_one(input: &[String]) -> u32 {
-    let (mut pattern, rules) = process_inputs(input);
-    for _ in 0..10 {
-        let mut output = "".to_string();
-        for idx in 0..pattern.len() - 1 {
-            let substring = &pattern[idx..idx + 2];
-            let repl: Vec<String> = rules
+fn do_work(input: &[String], n_iters: u32) -> u128 {
+    let (pattern, rules) = process_inputs(input);
+    // Parse pattern into a vector of patterns that match rules
+    let mut existing_patterns = HashMap::new();
+    for idx in 0..pattern.len() - 1 {
+        let substring = &pattern[idx..idx + 2];
+        if let Some(counter) = existing_patterns.get_mut(&substring.to_string()) {
+            *counter += 1;
+        } else {
+            existing_patterns.insert(substring.to_string(), 1);
+        }
+    }
+
+    for _ in 0..n_iters {
+        let mut patterns = HashMap::new();
+        existing_patterns.iter().for_each(|(ch, count)| {
+            let repl: Vec<Vec<String>> = rules
                 .iter()
-                .map(|x| x.matches(substring))
+                .map(|x| x.gen_new_pattern(ch))
                 .flatten()
                 .collect();
-            output = format!("{}{}{}", output, substring.chars().next().unwrap(), repl[0]);
-        }
-        output = format!(
-            "{}{}",
-            output,
-            pattern.chars().nth(pattern.len() - 1).unwrap()
-        );
-        pattern = output;
+            repl[0].iter().for_each(|x| {
+                if let Some(counter) = patterns.get_mut(x) {
+                    *counter += *count;
+                } else {
+                    patterns.insert(x.to_owned(), *count);
+                }
+            })
+        });
+        existing_patterns = patterns;
     }
-    let mut counter = HashMap::new();
-    pattern.chars().for_each(|x| {
-        if let Some(c) = counter.get_mut(&x) {
-            *c += 1;
-        } else {
-            counter.insert(x, 1);
-        }
+    let mut alphabet = HashMap::new();
+    existing_patterns.iter().for_each(|(patt, count)| {
+        patt.chars().for_each(|c| {
+            if let Some(counter) = alphabet.get_mut(&c) {
+                *counter += *count;
+            } else {
+                alphabet.insert(c, *count);
+            }
+        });
     });
-    // Create a reverse hashmap
+    // Divide by two and round down to actually get the numbers
+    alphabet.iter_mut().for_each(|(_, count)| {
+        *count /= 2;
+    });
+    *alphabet.get_mut(&pattern.chars().next().unwrap()).unwrap() += 1;
+    *alphabet
+        .get_mut(&pattern.chars().nth(pattern.len() - 1).unwrap())
+        .unwrap() += 1;
     let mut vec = Vec::new();
-    counter.iter().for_each(|(char, count)| {
+    alphabet.iter().for_each(|(char, count)| {
         vec.push((char, count));
     });
     vec.sort_by(|a, b| a.1.partial_cmp(b.1).unwrap());
     vec[vec.len() - 1].1 - vec[0].1
 }
 
+fn part_one(input: &[String]) -> u32 {
+    do_work(input, 10) as u32
+}
+
 fn part_two(input: &[String]) -> u128 {
-    let (mut pattern, rules) = process_inputs(input);
-    for _ in 0..40 {
-        let mut output = "".to_string();
-        for idx in 0..pattern.len() - 1 {
-            let substring = &pattern[idx..idx + 2];
-            let repl: Vec<String> = rules
-                .iter()
-                .map(|x| x.matches(substring))
-                .flatten()
-                .collect();
-            output = format!("{}{}{}", output, substring.chars().next().unwrap(), repl[0]);
-        }
-        output = format!(
-            "{}{}",
-            output,
-            pattern.chars().nth(pattern.len() - 1).unwrap()
-        );
-        pattern = output;
-    }
-    let mut counter = HashMap::new();
-    pattern.chars().for_each(|x| {
-        if let Some(c) = counter.get_mut(&x) {
-            *c += 1;
-        } else {
-            counter.insert(x, 1);
-        }
-    });
-    // Create a reverse hashmap
-    let mut vec = Vec::new();
-    counter.iter().for_each(|(char, count)| {
-        vec.push((char, count));
-    });
-    vec.sort_by(|a, b| a.1.partial_cmp(b.1).unwrap());
-    vec[vec.len() - 1].1 - vec[0].1
+    do_work(input, 40)
 }
 
 #[cfg(test)]
@@ -147,12 +142,12 @@ CN -> C"
         assert_eq!(output, truth);
     }
 
-    // #[test]
-    // fn test_two() {
-    //     let input = input();
-    //     let output = part_two(&str_to_string_vec(&input));
-    //     let truth = 2188189693529u128;
+    #[test]
+    fn test_two() {
+        let input = input();
+        let output = part_two(&str_to_string_vec(&input));
+        let truth = 2188189693529u128;
 
-    //     assert_eq!(output, truth);
-    // }
+        assert_eq!(output, truth);
+    }
 }
